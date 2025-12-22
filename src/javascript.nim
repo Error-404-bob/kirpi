d91660f5f6617f5c7a895b332784ff49978a35cc
Eray Zesen <erayzesen@gmail.com>
2025-12-22 12:10:32 +0300

    Added Javascript section to the cheatsheet.

--- src/javascript.nim
+++ src/javascript.nim
@@ -1,6 +1,3 @@
-#region Javascript
-
-import json
 
 type
     JSCallback* = proc(arg: cstring) {.cdecl.}      # For JSON argument callbacks(string)
@@ -58,45 +55,34 @@ when defined(emscripten):
     proc kirpi_create_callback_void(cb: JSCallbackVoid, eventName: cstring): cint {.importc, nodecl.}
 
     # Eval Functions
-    proc emscripten_run_script_int(script: cstring): cint {.importc, header: "<emscripten/emscripten.h>".}
-    proc emscripten_run_script_double(script: cstring): cdouble {.importc, header: "<emscripten/emscripten.h>".}
-    proc emscripten_run_script_bool(script: cstring): cint {.importc, header: "<emscripten/emscripten.h>".}
     proc emscripten_run_script_string(script: cstring): cstring {.importc, header: "<emscripten/emscripten.h>".}
-    proc emscripten_run_script(script: cstring): void {.importc, header: "<emscripten/emscripten.h>".}
+    #---Maybe useful later
+    #proc emscripten_run_script_int(script: cstring): cint {.importc, header: "<emscripten/emscripten.h>".}
+    #proc emscripten_run_script_double(script: cstring): cdouble {.importc, header: "<emscripten/emscripten.h>".}
+    #proc emscripten_run_script_bool(script: cstring): cint {.importc, header: "<emscripten/emscripten.h>".}
+    #proc emscripten_run_script(script: cstring): void {.importc, header: "<emscripten/emscripten.h>".}
 
 proc cstring2String(cstr: cstring): string =
   if cstr == nil: ""
   else: $cstr
 
 # Json Returning Callback
-proc createCallback*(cb: JSCallback; jsEvent: cstring): cint =
+proc createCallback*(cb: JSCallback; jsEvent: cstring) =
     when defined(emscripten):
-        result = kirpi_create_callback(cb, jsEvent)
-    else: result = 0
+        discard kirpi_create_callback(cb, jsEvent)
+    else: discard
 
 # Void Callback
-proc createCallback*(cb: JSCallbackVoid; jsEvent: cstring): cint =
+proc createCallback*(cb: JSCallbackVoid; jsEvent: cstring) =
     when defined(emscripten):
-        result = kirpi_create_callback_void(cb, jsEvent)
-    else: result = 0
+        discard kirpi_create_callback_void(cb, jsEvent)
+    else: discard
 
 # generic eval
-proc eval(code: string): string =
+proc eval*(code: string): string =
   when defined(emscripten):
     result = emscripten_run_script_string(code.cstring).cstring2String()
   else:
     result = ""
 
-# test callback
-# 1. Senaryo: Veri lazım
-proc onDetailedClick(jsonArgs: cstring) {.cdecl.} =
-    echo "Detaylı veri geldi: ", $jsonArgs
 
-# 2. Senaryo: Sadece tıklandığını bilsem yeter
-proc onSimpleClick() {.cdecl.} =
-    echo "Sadece tıklandı, JSON ile vakit kaybetmedik."
-
-discard createCallback(onDetailedClick, "contextmenu") # Sağ tık, detaylı
-discard createCallback(onSimpleClick, "click")         # Sol tık, hızlı
-
-#endregion
--- src/kirpi.nim
+++ src/kirpi.nim
@@ -4,7 +4,7 @@ import tables
 import raylib as rl
 import rsc
 import hashes
-import bridge
+import javascript
 
 
 #Emscripten /Web Main Loop Fix Wrapper (We don't want to use Asyncify on the web targets)
@@ -205,4 +205,4 @@ proc run*(title:string,load: proc(), update: proc(dt:float), draw: proc(), confi
 export graphics except defaultFilter,shaders,fonts
 export inputs, window
 export sound 
-export bridge
+export javascript
